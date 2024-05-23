8a940aa8e601d5c7510f415f5c3a05a4f15e320c
Gabriel Ribeiro <Gabrielribeiro.dev23@gmail.com>
2024-05-23 15:24:13 -0300

    Armazenar o número secreto e o número fornecido pelo usuário

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ alert("Seja bem-vindo(A) ao jogo do número secreto!");
 
 // Declarando variáveis
 
-let numeroSecreto = 29;
-prompt("Escolha um número entre 1 e 100");
+let numeroSecreto = 5;
+let numeroChute = prompt("Escolha um número entre 1 e 10");
