63d2a5875ee389576a364e46be230192f50721f0
Gabriel Ribeiro <Gabrielribeiro.dev23@gmail.com>
2024-05-23 15:42:57 -0300

    Criada a condição que determina se o número fornecido pelo usuário é = ao número secreto

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ alert("Seja bem-vindo(A) ao jogo do número secreto!");
 
 let numeroSecreto = 5;
 let numeroChute = prompt("Escolha um número entre 1 e 10");
+
+// Comparando números
+
+if (numeroChute==numeroSecreto) {
+    console.log("Você descobriu o número secreto (5)!");
+} else {
+    console.log("Você errou!");
+}
