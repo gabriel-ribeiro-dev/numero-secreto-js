2255397241a1ae50b7fb24e637150bf6d3869c3c
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-05-30 15:58:31 -0300

    Adicionada a funcionalidade para obter mais de uma tentativa sem precisar reiniciar a cada rodada

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ alert("Seja bem-vindo(A) ao jogo do número secreto!");
 // Declarando variáveis
 
 let numeroSecreto = 5;
-let numeroChute = prompt("Escolha um número entre 1 e 10");
-
+let numeroChute;
+// Enquanto o númeroChute não for = ao número secreto
+while (numeroChute!=numeroSecreto) {
+    numeroChute = prompt("Escolha um número de 1 a 10");
 // Comparando números
 
 if (numeroChute==numeroSecreto) {
@@ -15,4 +17,5 @@ if (numeroChute==numeroSecreto) {
     alert(`Você errou! o número ${numeroChute} é maior do que o número secreto`);
 } else {
     alert(` Você errou! O número ${numeroChute} é menor do que o número secreto`);
+}
 }
