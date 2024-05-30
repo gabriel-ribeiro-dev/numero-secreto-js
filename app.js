9844b50d713f3bdf71b33277791107f37331983b
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-05-30 17:26:27 -0300

    Adicionada uma verificação que determina plural ou singular para a palavra tentativa dependendo da quantidade de tentativas que o usuário levou para acertar

--- app.js
+++ app.js
@@ -22,8 +22,5 @@ break;
 tentativas++;
 
 }
-if (tentativas>1) {
-    alert(`Parabéns! Você descobriu o número secreto! (${numeroSecreto} com ${tentativas} tentativas)`);
-} else {
-    alert(`Parabéns! Você descobriu o número secreto! (${numeroSecreto} com ${tentativas} tentativa)`);
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você descobriu o número secreto! (${numeroSecreto}) com ${tentativas} ${palavraTentativa}`);
