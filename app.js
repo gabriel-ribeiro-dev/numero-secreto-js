d37d12e00146e0625ff7630b0fd988d909b7e04c
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-05-30 15:18:36 -0300

    Inplementadas as dicas de erro do número

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ let numeroChute = prompt("Escolha um número entre 1 e 10");
 // Comparando números
 
 if (numeroChute==numeroSecreto) {
-    console.log("Você descobriu o número secreto (5)!");
+    alert(`Parabéns! Você descobriu o número secreto! (${numeroSecreto})`);
+} else  if (numeroChute>numeroSecreto){
+    alert(`Você errou! o número ${numeroChute} é maior do que o número secreto`);
 } else {
-    console.log("Você errou!");
+    alert(` Você errou! O número ${numeroChute} é menor do que o número secreto`);
 }
