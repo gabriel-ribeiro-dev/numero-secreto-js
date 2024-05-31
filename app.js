895c49b5fe6e2417454b6eb305b5709d8219381d
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-05-31 14:17:08 -0300

    Adicionada a função de gerar números aleatórios

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ alert("Seja bem-vindo(A) ao jogo do número secreto!");
 
 // Declarando variáveis
 
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 let numeroChute;
 let tentativas = 1;
 // Enquanto o númeroChute não for = ao número secreto
 while (numeroChute!=numeroSecreto) {
-    numeroChute = prompt("Escolha um número de 1 a 10");
+    numeroChute = prompt("Escolha um número de 1 a 100");
 // Comparando números
 
 if (numeroChute==numeroSecreto) {
