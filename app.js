b85afcb64843cd2cebdd8e70ddd97dfb21a41000
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-05-31 14:38:36 -0300

    Criado um dinamismo na escolha de números

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ alert("Seja bem-vindo(A) ao jogo do número secreto!");
 
 // Declarando variáveis
 
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let numeroChute;
 let tentativas = 1;
 // Enquanto o númeroChute não for = ao número secreto
 while (numeroChute!=numeroSecreto) {
-    numeroChute = prompt("Escolha um número de 1 a 100");
+    numeroChute = prompt(`Escolha um número de 1 até ${numeroMaximo}`);
 // Comparando números
 
 if (numeroChute==numeroSecreto) {
