cd52132bb9092292fdd5207bae2e91ab85d0901b
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-09-14 15:41:30 -0300

    Agora, é exibido uma mensagem de vitória ou derrota, dependendo do acerto ou erro do número.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert ('Seja bem-vindo(A) ao jogo do Número Secreto!');
+
+    // Mensagem de boas vindas
+    alert ('Seja bem-vindo(A) ao jogo do Número Secreto!');
+
+    // Declarando variáveis
+    let numeroSecreto = 5;
+
+    // Exibir o número no console
+console.log (numeroSecreto);
+
+// Obter o chute do usuário
+
+    let numeroChute = prompt ('Escolha um número entre 1 e 10');
+
+    // Comparando o chute  com o número secreto e exibindo a mensagem de vitória ou derrota na tela
+    if (numeroChute===numeroSecreto) {
+     alert ('Parabéns! Você descobriu o número secreto! (5)');
+    } else {
+        alert ('Você errou!');
+    }
