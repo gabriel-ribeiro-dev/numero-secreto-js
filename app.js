9e3524878064bf77ce43de2a0a73e30a54ab290a
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-09-15 14:51:35 -0300

    Implementada a nova feature que determina se exibe tentativas ou tentativa ao ganhar o jogo, dependendo da quantidade de erros

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
     // Declarando variável do número secreto
     let numeroSecreto = 4;
 
+    // Declarando a variável de controlhe de tentativas
+    let tentativas = 1;
     // Exibir o número no console
 console.log (numeroSecreto);
 
@@ -15,10 +17,17 @@ while (numeroChute!=numeroSecreto) {
     numeroChute = prompt ('Escolha um número entre 1 e 10');
         // Comparando o chute  com o número secreto e exibindo a mensagem de vitória ou derrota na tela
         if (numeroChute==numeroSecreto) {
-            alert (`Parabéns! Você descobriu o número secreto! (${numeroSecreto})`);
-           } else if (numeroChute>numeroSecreto) {
+break;
+        } else if (numeroChute>numeroSecreto) {
        alert (`O número ${numeroChute} é maior do que o número secreto!`);
            } else {
                alert (`O número ${numeroChute} é menor do que o número secreto!`);
            }
-}
+           tentativas++;
+
+        }
+        
+
+        // Palavra tentativas e exebição da mensagem correta
+        let PalavraTentativas = tentativas>1? 'Tentativas' : 'tentativa';
+        alert (`Parabéns! Você descobriu o número secreto! (${numeroSecreto}) com ${tentativas} ${PalavraTentativas}`);
