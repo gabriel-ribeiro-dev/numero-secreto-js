dd5cd0879e5082e3b85cd3a209b381fc204bef60
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-09-30 21:12:47 -0300

    Criadas as frazes de boas vindas ao jogo

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML= 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número de 1 até 10';
