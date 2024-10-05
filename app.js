5f680537b866f23d237c515110372a26c9d7c69d
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2024-10-05 17:56:00 -0300

    Implementada a funcionalidade que permite a leitura por voz do jogo para o idioma português

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function exibirMensagemInicial() {
     function exibirTextoNaTela(tag, texto) {
         let campo = document.querySelector(tag);
         campo.innerHTML = texto;
+        responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
     }
 
     exibirMensagemInicial();
